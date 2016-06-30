47a9af4c1cbf31a811e82eeb19492aa749255d6d
Andreza Feu <andrezafeu@gmail.com>
2016-06-30 14:28:32 -0400

    Append new color to the color list

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-$("#color-list-js li").click(function(){
+$("#color-list-js").on("click", "li", function(){
+	// this li
 	$(this).siblings().removeClass("selected");
 	$(this).addClass("selected");
 	color = $(this).css("background-color");
@@ -9,6 +10,7 @@ $("#revealColorSelect").click(function(){
 	$("#colorSelect").toggle();
 });
 
+
 function changeColor() {
 	var r = $("#red").val();
 	var g = $("#green").val();
@@ -18,3 +20,9 @@ function changeColor() {
 
 // get the sliders value
 $("input[type=range]").on("input", changeColor);
+
+$("#addNewColor").click(function() {
+	var $newColor = $("<li></li>");
+	$newColor.css("background-color", $("#newColor").css("background-color"));
+	$("#color-list-js").append($newColor);
+});
