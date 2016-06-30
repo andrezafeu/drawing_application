b536891fea13dfe0613ad870f79caa396a596158
Andreza Feu <andrezafeu@gmail.com>
2016-06-30 13:18:19 -0400

    Change new color when moving the sliders

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
 $("#color-list-js li").click(function(){
-	$("#color-list-js > li").removeClass("selected");
-	$(this).toggleClass("selected");
+	$(this).siblings().removeClass("selected");
+	$(this).addClass("selected");
+	color = $(this).css("background-color");
 });
 
 $("#revealColorSelect").click(function(){
+	changeColor();
 	$("#colorSelect").toggle();
 });
+
+function changeColor() {
+	var r = $("#red").val();
+	var g = $("#green").val();
+	var b = $("#blue").val();	
+	$("#newColor").css("background-color", "rgb("+ r + "," + g + "," + b + ")");
+}
+
+// get the sliders value
+$("input[type=range]").on("input", changeColor);
