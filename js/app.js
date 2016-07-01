ce8500e8e407875ad82bb2e81edd2d030d16880f
Andreza Feu <andrezafeu@gmail.com>
2016-07-01 11:46:45 -0400

    Allows to drawn on canvas

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+var color = $(".selected").css("background-color");
+var $canvas = $("canvas");
+// Grab the first element in the array of canvas elements
+// It's necessary to grab a html element because of the method to be called on it
+// document.getElementByTagName("canvas")[0]
+var context = $canvas[0].getContext("2d");
+var lastEvent;
+var mouseDown = false;
+
 $("#color-list-js").on("click", "li", function(){
 	// this li
 	$(this).siblings().removeClass("selected");
@@ -9,7 +18,6 @@ $("#revealColorSelect").click(function(){
 	changeColor();
 	$("#colorSelect").toggle();
 });
-
 
 function changeColor() {
 	var r = $("#red").val();
@@ -25,4 +33,27 @@ $("#addNewColor").click(function() {
 	var $newColor = $("<li></li>");
 	$newColor.css("background-color", $("#newColor").css("background-color"));
 	$("#color-list-js").append($newColor);
+});
+
+$canvas.mousedown(function(e) {
+	lastEvent = e;
+	mouseDown = true;
+}).mousemove(function(e){
+	if (mouseDown) {
+		// Defines the beginning of the path
+		context.beginPath();
+		// Define where to start the path
+		context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+		// Draws a line
+		context.lineTo(e.offsetX, e.offsetY);
+		context.strokeStyle = color;
+		context.stroke();
+		lastEvent = e;
+	}
+
+}).mouseup(function(){
+	mouseDown = false;
+}).mouseleave(function(){
+	// if the mouse leaves the canvas, it triggers mouseup, correcting bug
+	$canvas.mouseup();
 });
